9b28da8ed00c5e8f1a6075dd0bc48915feafd6fb
JanapanaSateesh <47514442+JanapanaSateesh@users.noreply.github.com>
2024-08-11 12:40:54 +0530

    Update Base.java

--- src/test/java/com/orangehrm/testcases/Base.java
+++ src/test/java/com/orangehrm/testcases/Base.java
@@ -42,7 +42,7 @@ public class Base  {
 		options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
 		//After Conflicts
 
-		driver=new ChromeDriver();
+		driver=new ChromeDriver(options);
 		driver.get(url);
 		driver.manage().window().maximize();	
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
