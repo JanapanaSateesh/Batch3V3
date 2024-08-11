aaf6c7a78d43ac9a82710ea3b7ef9716698331ef
JanapanaSateesh <47514442+JanapanaSateesh@users.noreply.github.com>
2024-08-11 11:59:58 +0530

    Update Base.java

--- src/test/java/com/orangehrm/testcases/Base.java
+++ src/test/java/com/orangehrm/testcases/Base.java
@@ -35,8 +35,11 @@ public class Base  {
 		url=credentials.getUrl();
 		username=credentials.getusername();
 		password=credentials.getPassword();
+
+		ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
 		
-		driver=new ChromeDriver();
+		driver=new ChromeDriver(options);
 		driver.get(url);
 		driver.manage().window().maximize();	
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
