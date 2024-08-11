79d60774af591c82fbff04d751dce42644b1097e
JanapanaSateesh <47514442+JanapanaSateesh@users.noreply.github.com>
2024-08-11 12:02:31 +0530

    Update Base.java

--- src/test/java/com/orangehrm/testcases/Base.java
+++ src/test/java/com/orangehrm/testcases/Base.java
@@ -6,6 +6,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
